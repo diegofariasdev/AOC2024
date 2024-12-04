d0be19596865c8c7b122263996e6d512c4e301ad
Diego Farias <diego@ideaslibres.net>
2024-12-04 14:25:51 -0600

    Add day 4 challente 2

--- source/day_4.py
+++ source/day_4.py
@@ -1,5 +1,5 @@
-def read_input(test_mode):
-	if test_mode:
+def read_input(test_mode, ex=1):
+	if test_mode and ex == 1:
 		return [
 			'MMMSXXMASM',
 			'MSAMXMSMSA',
@@ -11,6 +11,10 @@ def read_input(test_mode):
 			'SAXAMASAAA',
 			'MAMMMXMMMM',
 			'MXMXAXMASX',
+		]
+	if test_mode and ex == 2:
+		return [
+
 		]
 	content = []
 	with open('data/data_day_4_1.txt') as f:
@@ -39,6 +43,34 @@ def count_xmas(letter_matrix, start_row, start_col):
 				counter += 1
 	return counter
 
+def count_xmas_2(letter_matrix, start_row, start_col):
+	counter = 0
+	for m_row in [-1,1]:
+		for m_col in [-1,1]:
+			if len(letter_matrix) <= start_row + m_row:
+				continue
+			if start_row + m_row < 0:
+				continue
+			if len(letter_matrix[start_row + m_row]) <= start_col + m_col:
+				continue
+			if start_col + m_col < 0:
+				continue
+			if letter_matrix[start_row + m_row][start_col + m_col] != 'M':
+				continue
+			s_row = m_row * -1
+			s_col = m_col * -1
+			if len(letter_matrix) <= start_row + s_row:
+				continue
+			if start_row + s_row < 0:
+				continue
+			if len(letter_matrix[start_row + s_row]) <= start_col + s_col:
+				continue
+			if start_col + s_col < 0:
+				continue
+			if letter_matrix[start_row + s_row][start_col + s_col] != 'S':
+				continue
+			counter += 1
+	return 1 if counter > 1 else 0
 
 def day4_1(test_mode):
 	letter_matrix = read_input(test_mode)
@@ -48,3 +80,12 @@ def day4_1(test_mode):
 			if letter_matrix[i][j] == "X":
 				total_xmas += count_xmas(letter_matrix, i, j)
 	print('total xmas ocurrences is {}'.format(total_xmas))
+
+def day4_2(test_mode):
+	letter_matrix = read_input(test_mode)
+	total_xmas = 0
+	for i in range(len(letter_matrix)):
+		for j in range(len(letter_matrix)):
+			if letter_matrix[i][j] == 'A':
+				total_xmas += count_xmas_2(letter_matrix, i, j)
+	print('total x-mas ocurrences is {}'.format(total_xmas))
