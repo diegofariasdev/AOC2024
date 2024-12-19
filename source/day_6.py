9e3ca8c776658a7da97051b2c8b37c0cbec88084
Diego Farias <diego@ideaslibres.net>
2024-12-18 21:40:49 -0600

    Add day 6 challenge 2

--- source/day_6.py
+++ source/day_6.py
@@ -124,54 +124,69 @@ def day6_1(test_mode):
 			break
 	print("The sum of unique positions is {}".format(len(unique_positions)))
 
-def calculate_seg_lenght(end1, end2):
-	return abs(end2[0] - end1[0]) + abs(end2[1] - end1[1])
-
-def is_point_on_line(end1, end2, point):
-	return (end2[0] - end1[0]) * (point[1] - end1[1]) ==\
-		(end2[1] - end1[1]) * (point[0] - end1[0])
-
 """
-idea: a new blocker can create a loop if the third path segment is longer than the first one,
-and the guard was not in the third segment.
-A window can be created to store three segments at a time, and stepping one by one, 
-in each step it should validate that the third segment is longer than the first one, and that
-the guard was not in the third segment initially, and if those two conditions are true, we can
-add 1 to the sum of places where a new blocker can be put to create a loop
-guard pos: (gr, gc)
-window: [((r1,c1),(r2,c2),(r3,c3),(r4,c4))]
-first segment long = abs(r1 - r2) + abs(c1 - c2)
-third segment long = abs(r3 - r4) + abs(c3 - c4)
-guard is in a segment? (r4 - r3) * (gc - c3) == (c4 - c3) * (gr - r3)
+Segment by segment evaluation
+Steps: take current positon, and the closest blocker, for each position in between them
+if no previous blocker has been added, add a blocker, and evaluate if a loop is made.
+if a loop occurs, add 1.
+
+to evaluate a loop, while blocker ahead, or edge ahead, if the blocker ahead has been
+hitted in the same direction in the past, it must be a loop. if not move forward until
+after the most closest blocker, and turn 90 degres to the right.
+if out of the while, return false as no loop has been found
 """
+def is_blocker_hit_already(hit_blockers, blocker, direction):
+	return blocker in hit_blockers and direction in hit_blockers[blocker]
+
+def does_guard_loops(map_widht, map_height, blockers_positions, guard_position):
+	direction = "up"
+	hit_blockers = {}
+	for i in range(map_widht * map_height): # setting a max of iterations
+		found_blockers = [blocker for blocker in blockers_positions \
+			if is_blocking_guard(guard_position, blocker, direction)]
+		if found_blockers:
+			found_blocker = get_closest_blocker(guard_position, found_blockers)
+
+			if is_blocker_hit_already(hit_blockers, found_blocker, direction):
+				print('loop found!!!')
+				return True
+			if found_blocker not in hit_blockers:
+				hit_blockers[found_blocker] = set()
+			if direction not in hit_blockers[found_blocker]:
+				hit_blockers[found_blocker].add(direction)
+
+			guard_position = new_guard_position(found_blocker, direction)
+			direction = rotate(direction)
+		else:
+			return False
+
 def day6_2(test_mode):
 	guard_map = read_input(test_mode)
 	guard_position, blockers_positions = parse_map(guard_map)
 	initial_guard_position = guard_position
 	direction = "up"
-	evaluating_window = [initial_guard_position]
-	total_valid_blockers = 0
-	for i in range(len(guard_map) * len(guard_map[0])): # setting a max of iterations
+	map_widht = len(guard_map)
+	map_height = len(guard_map[0])
+	looping_blockers = set()
+	for i in range(map_widht * map_height): # setting a max of iterations
 		found_blockers = [blocker for blocker in blockers_positions \
 			if is_blocking_guard(guard_position, blocker, direction)]
 		if found_blockers:
 			found_blocker = get_closest_blocker(guard_position, found_blockers)
+			for position in get_positions(guard_position, found_blocker):
+				new_blockers_position = blockers_positions + [position]
+				if does_guard_loops(map_widht, map_height, new_blockers_position, initial_guard_position):
+					looping_blockers.add(position)
+					print('A new blocker in {} loops the guard'.format(position))
 			guard_position = new_guard_position(found_blocker, direction)
-
-			if len(evaluating_window) < 4:
-				evaluating_window.append(guard_position)
-			if len(evaluating_window) == 4:
-				first_seg_len = calculate_seg_lenght(evaluating_window[0], evaluating_window[1])
-				third_seg_len = calculate_seg_lenght(evaluating_window[2], evaluating_window[3])
-				if first_seg_len >= third_seg_len and \
-					not is_point_on_line(evaluating_window[2], evaluating_window[3], initial_guard_position):
-					print("blocker on {} {}".format(evaluating_window[2], evaluating_window[3]))
-					total_valid_blockers += 1
-				evaluating_window.pop(0)
-
 			direction = rotate(direction)
 		else:
-			#unique_positions.update(get_positions(guard_position, \
-				#get_edge_position(guard_position, len(guard_map), len(guard_map[0]), direction)))
+			edge_position = get_edge_position(guard_position, map_widht + 1, map_height + 1, direction)
+			for position in get_positions(guard_position, edge_position):
+				new_blockers_position = blockers_positions + [position]
+				if does_guard_loops(map_widht, map_height, new_blockers_position, initial_guard_position):
+					looping_blockers.add(position)
+					print('A new blocker in {} loops the guard'.format(position))
 			break
-	print("The total of possible blockers is {}".format(total_valid_blockers))
+			looping_blockers.delete(initial_guard_position)
+	print('The total number of looping blockers is {}'.format(len(looping_blockers)))
